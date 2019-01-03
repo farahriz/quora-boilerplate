234377bcbe50a74d9c7f2d18d0242ed9a5d35c2b
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-03 11:50:31 +0800

    Added validations to question

--- controllers/questions_controller.rb
+++ controllers/questions_controller.rb
@@ -1,36 +1,40 @@
-
-get '/questions' do
-	# @questions = Question.all
-	erb :"questions/index"
-end
-
 get '/questions/new' do
 	erb :"questions/new"
 end
 
 post '/questions/new' do
-	@question = Question.create(:title => params[:title], :content => params[:content], :user_id => current_user)
+	@user = current_user
+	@question = Question.create(:title => params[:title], :content => params[:content], :user_id => @user.id)
 	redirect to "/questions/#{@question.id}"
 end
 
 get '/questions/:id' do
 	# => params[:id] returns the value input in the :wildcard path
+	@user = current_user
 	@question = Question.find(params[:id])
     erb :"questions/show"
 end
 
 
 
-get '/questions/:id/edit' do
-	@question = Question.find_by_id(params[:id])
-	erb :"questions/edit"
 
-end
+# The ones below here have not been implemented yet
+	# get '/questions' do
+	# 	# @questions = Question.all
+	# 	erb :"questions/index"
+	# end
 
-patch '/articles/:id' do
-	@question = Question.find_by_id(params[:id])
-	@question.title = params[:title]
-	@question.content = params[:content]
-	@question.save
-	redirect to "/questions/#{@question.id}"
-end
+
+	# get '/questions/:id/edit' do
+	# 	@question = Question.find(params[:id])
+	# 	erb :"questions/edit"
+
+	# end
+
+	# patch '/articles/:id' do
+	# 	@question = Question.find(params[:id])
+	# 	@question.title = params[:title]
+	# 	@question.content = params[:content]
+	# 	@question.save
+	# 	redirect to "/questions/#{@question.id}"
+	# end
--- models/question.rb
+++ models/question.rb
@@ -1,4 +1,8 @@
 class Question < ActiveRecord::Base
 	belongs_to :user
 
+	validates :user_id, prescence: true
+	validates :title, prescence: true
+	validates :content, prescence: true
+
 end
