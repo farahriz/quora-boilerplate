517d7b45243346c55a56308c30e301539d546d61
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-03 18:13:45 +0800

    Added user key to answers

--- db/migrate/20190103180813_add_user_key_to_answers.rb
+++ db/migrate/20190103180813_add_user_key_to_answers.rb
@@ -1,5 +1,5 @@
 class AddUserKeyToAnswers < ActiveRecord::Migration[4.2]
 	def change
-		add_reference :answers, :question, foreign_key: true
+		add_reference :answers, :user, foreign_key: true
 	end
 end
