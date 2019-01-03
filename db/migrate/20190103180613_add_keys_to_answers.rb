bb0dae708531a81aae20dd3616fa60039415d0ef
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-03 18:10:34 +0800

    Created answers table

--- db/migrate/20190103180613_add_keys_to_answers.rb
+++ db/migrate/20190103180613_add_keys_to_answers.rb
@@ -1,6 +1,5 @@
-class AddKeyUserToQuestions < ActiveRecord::Migration[4.2]
+class AddKeysToAnswers < ActiveRecord::Migration[4.2]
 	def change
 		add_reference :answers, :question, foreign_key: true
-		add_reference :answers, :user, foreign_key: true
 	end
 end
