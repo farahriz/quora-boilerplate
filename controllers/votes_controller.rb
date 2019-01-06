226f0922168af232cfa323f2d8d933353be3fa3e
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-07 00:08:21 +0800

    Added crude delete and edit for votes

--- controllers/votes_controller.rb
+++ controllers/votes_controller.rb
@@ -7,17 +7,41 @@ end
 
 #create a vote
 post '/votes/new' do
-		byebug
 		@vote = Vote.new(params[:vote])
 		if @vote.save
 			@vote.save
 		else
-			#redirect to an error message
+			p "There was an error"
 		end
 
 end
 
+
+#edit a vote
+get '/votes/:id/edit' do
+	@vote = Vote.find(params[:id])
+	erb :"votes/edit"
+
+end
+
+patch '/votes/:id/edit' do
+	# byebug
+	@vote = Vote.find(params[:id])
+	@vote.update(params[:vote])
+	@vote.save
+	redirect to "/votes"
+end
+
+
 #remove a vote
-post '/votes/delete' do
-	
+get '/votes/:id/delete' do
+	@vote = Vote.find(params[:id])
+	erb :"votes/delete"
+end
+
+
+delete '/votes/:id/delete' do
+	@vote = Vote.find(params[:id])
+	@vote.delete
+	redirect to '/votes'
 end
